64310e590f4094b5a4156a42e8035a8a13dd98c3
Arek <arek-karczo.wp.pl@wp.pl>
2024-12-29 12:48:01 +0100

    none'V2

--- index.js
+++ index.js
@@ -5,16 +5,37 @@ const tweetBtn = document.getElementById("tweet-btn")
 // const feed = document.getElementById("feed")
 
 
-tweetBtn.addEventListener('click', function(){
-    
-    
+tweetBtn.addEventListener('click', function(){       
+})
+
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    }
 })
 
 
+function handleLikeClick(tweetId) {
+    
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+  
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    } else  {
+        targetTweetObj.likes++
+    }
+
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    
+    render()
+}
 
 
 function getFeedHtml() {
     let feedHtml = ``
+
     tweetsData.forEach(function (tweet) {
         feedHtml += `
         <div class="tweet">
@@ -25,12 +46,21 @@ function getFeedHtml() {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular 
+                            fa-comment-dots" 
+                            data-reply="${tweet.uuid}"></i>
                                     ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid 
+                            fa-heart" 
+                            data-like="${tweet.uuid}"></i>
                                     ${tweet.likes} 
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid 
+                            fa-retweet" 
+                            data-retweet="${tweet.uuid}"></i>
                                     ${tweet.retweets}
                         </span>
                     </div>   
